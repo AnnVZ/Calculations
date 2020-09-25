37e3f79fc315ab380c882dd35005cbb91a6faec4
AnnVZ <ann.zhlbch@gmail.com>
2020-09-25 18:19:35 +0300

    step 2 update

--- src/main/java/com/example/hw3/CalcController.java
+++ src/main/java/com/example/hw3/CalcController.java
@@ -5,9 +5,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 @Controller
 public class CalcController {
+    private static String pattern = "###,###.######";
+    private static DecimalFormat decimalFormat = new DecimalFormat(pattern);
 
     @GetMapping("/")
     public String getIndex(Model model) {
@@ -19,14 +23,34 @@ public class CalcController {
     public String getSum(@RequestParam(value = "number1", defaultValue = "0") String number1,
                          @RequestParam(value = "number2", defaultValue = "0") String number2,
                          Model model) {
-        return (new BigDecimal(number1).add(new BigDecimal(number2))).toString();
+        return setFormat((new BigDecimal(number1).add(new BigDecimal(number2))));
     }
 
-    @GetMapping("/diff")
+    @GetMapping("/dif")
     @ResponseBody
     public String getDiff(@RequestParam(value = "number1", defaultValue = "0") String number1,
                          @RequestParam(value = "number2", defaultValue = "0") String number2,
                          Model model) {
-        return (new BigDecimal(number1).subtract(new BigDecimal(number2))).toString();
+        return setFormat((new BigDecimal(number1).subtract(new BigDecimal(number2))));
+    }
+
+    @GetMapping("/mult")
+    @ResponseBody
+    public String getMult(@RequestParam(value = "number1", defaultValue = "0") String number1,
+                          @RequestParam(value = "number2", defaultValue = "0") String number2,
+                          Model model) {
+        return setFormat((new BigDecimal(number1).multiply(new BigDecimal(number2))));
+    }
+
+    @GetMapping("/div")
+    @ResponseBody
+    public String getDiv(@RequestParam(value = "number1", defaultValue = "0") String number1,
+                          @RequestParam(value = "number2", defaultValue = "0") String number2,
+                          Model model) {
+        return setFormat((new BigDecimal(number1).divide(new BigDecimal(number2), 6, RoundingMode.HALF_UP)));
+    }
+
+    private String setFormat(BigDecimal number) {
+        return decimalFormat.format(number.setScale(6, RoundingMode.HALF_UP));
     }
 }
